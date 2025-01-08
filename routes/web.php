08e0c959295a8c069e613c6ae273b3bd6e5063e8
Jarrod Ferriss <w0856197@myscc.ca>
2025-01-08 11:33:45 -0500

    Assignment #1 - Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 5.
+Route::get('contact', function () {
+    return "My name is Jarrod Ferriss.";
+});
+
+// 6.
+Route::get('easy', function () {
+    return "Laravel makes it easy to develop websites!";
+});
+
+// 7.
+Route::get('uid/{id}', function ($id) {
+    return "ID: $id";
+}) -> where(['id' => '[0-9]+']);
+
+// 10.
+Route::group(['as' => 'users.', 'prefix' => 'users', 'where' => ['name' => '[A-Z a-z]+'],
+    ], function () {
+
+    // 8.
+    Route::get('{name?}', function ($name = "Batman"){
+        return "Name: $name";
+    }) -> name('show');
+
+    // 9.
+    Route::get('{name?}/images/{id?}', function ($name = "Batman", $id = "1") {
+        return "Name: $name Image: $id";
+    }) -> where(['id' => '[0-9]+']) -> name('images.show');
+});
